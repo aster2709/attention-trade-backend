59dc8bed6e934efae74fe3185fc630a9caacd770
aster2709 <ymehta777@gmail.com>
2025-08-25 02:24:59 +0530

    update criterias

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 import { config } from "./env";
+import { UserModel } from "../models/user.model";
 
 export const connectDB = async () => {
   try {
     // No need to check for existence here, it's already validated
     await mongoose.connect(config.MONGO_URI);
+	await UserModel.syncIndexes();
     console.log("MongoDB Connected...");
   } catch (err: any) {
     console.error(`MongoDB Connection Error: ${err.message}`);
